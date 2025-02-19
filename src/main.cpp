d5cd373725f3d876c7594a6559e76f0e762dd841
prevter <prevter@gmail.com>
2025-02-19 19:32:06 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <Geode/Geode.hpp>
+using namespace geode::prelude;
+
+$on_mod(Loaded) {
+    auto base = base::get();
+    auto res = Mod::get()->patch(
+    GEODE_WINDOWS(
+        reinterpret_cast<void*>(base + 0x4b6d54),
+        { 0xe9, 0x8a, 0x00, 0x00, 0x00 }
+    )
+    GEODE_INTEL_MAC(
+        reinterpret_cast<void*>(base + 0x7dc3),
+        {
+            0x90, 0x90, 0x90, 0x90, 0x90, 0x90,
+            0x90, 0x90, 0x90, 0x90, 0x90, 0x90
+            0x90, 0x90, 0x90, 0x90
+        }
+    )
+    GEODE_ARM_MAC(
+        reinterpret_cast<void*>(base + 0x9ac8),
+        {
+            0x1F, 0x20, 0x03, 0xD5,
+            0x1F, 0x20, 0x03, 0xD5
+        }
+    )
+    );
+    if (!res) { log::error("Failed to patch"); }
+}
