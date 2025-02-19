2a53c1824aeab986f7a2b329260f90774573f47a
prevter <prevter@gmail.com>
2025-02-19 21:36:43 +0200

    future proof with sigscanning

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,53 @@
+#include <sinaps.hpp>
 #include <Geode/Geode.hpp>
 using namespace geode::prelude;
 
+#ifdef GEODE_IS_WINDOWS
+    #include <psapi.h>
+#endif
+
+size_t baseSize() {
+    static size_t size = []() -> size_t {
+    #ifdef GEODE_IS_WINDOWS
+        auto handle = GetModuleHandle(nullptr);
+        if (!handle) return 0;
+
+        MODULEINFO info;
+        if (!GetModuleInformation(GetCurrentProcess(), handle, &info, sizeof(info)))
+            return 0;
+
+        return info.SizeOfImage;
+    #elif defined(GEODE_IS_INTEL_MAC)
+        return 0x980000;
+    #elif defined(GEODE_IS_ARM_MAC)
+        return 0x8B0000;
+    #else
+        #error "Unsupported platform"
+    #endif
+    }();
+    return size;
+}
+
 $on_mod(Loaded) {
-    auto base = base::get();
-    auto res = Mod::get()->patch(
-    GEODE_WINDOWS(
-        reinterpret_cast<void*>(base + 0x4b6d54),
-        { 0xe9, 0x8a, 0x00, 0x00, 0x00 }
-    )
-    GEODE_INTEL_MAC(
-        reinterpret_cast<void*>(base + 0x7dc3),
-        {
-            0x90, 0x90, 0x90, 0x90, 0x90, 0x90,
-            0x90, 0x90, 0x90, 0x90, 0x90, 0x90,
-            0x90, 0x90, 0x90, 0x90
-        }
-    )
-    GEODE_ARM_MAC(
-        reinterpret_cast<void*>(base + 0x9ac8),
-        {
-            0x1F, 0x20, 0x03, 0xD5,
-            0x1F, 0x20, 0x03, 0xD5
-        }
-    )
-    );
-    if (!res) { log::error("Failed to patch"); }
+    auto base = reinterpret_cast<uint8_t*>(base::get());
+    auto size = baseSize();
+
+    auto offset = sinaps::find<
+        GEODE_WINDOWS("48 8B D8 ^ FF 15 ? ? ? ? 85 C0 74 11")
+        GEODE_INTEL_MAC("E8 ? ? ? ? 48 89 C7 ^ E8 ? ? ? ? 84 C0 0F 84 ? ? ? ? 4C 8D 73")
+        GEODE_ARM_MAC("22 00 80 52 ? ? ? ? ? ? ? ? ^ ? ? ? ? ? ? 00 36")
+    >(base, size);
+
+    if (offset == -1) return log::error("Failed to find address");
+
+    auto addr = reinterpret_cast<void*>(base + offset);
+    std::vector<uint8_t> bytes = {
+        GEODE_WINDOWS(0xe9, 0x8a, 0x00, 0x00, 0x00)
+        GEODE_INTEL_MAC(0xb0, 0x01, 0x90, 0x90, 0x90)
+        GEODE_ARM_MAC(0x1F, 0x20, 0x03, 0xD5, 0x1F, 0x20, 0x03, 0xD5)
+    };
+
+    if (!Mod::get()->patch(addr, bytes)) {
+        log::error("Failed to patch");
+    }
 }
